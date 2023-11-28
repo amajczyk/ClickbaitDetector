45cdb46898d4d00029fe0d15b920ffff0909897c
amajczyk <adam.majczyk2001@gmail.com>
2023-11-28 16:00:57 +0100

    add fixed preprocessing

--- modelling/classes.py
+++ modelling/classes.py
@@ -4,7 +4,6 @@ class Word2VecModel():
     def __init__(self, model_settings) -> None:
         self.model = Word2Vec.load(model_settings['model_path'])
         self.model_settings = model_settings
-        self.vector_size = self.model_settings['vector_size']
         self.is_skipgram = self.model_settings['is_skipgram']
         self.window_size = self.model_settings['window_size']
         self.vector_size = self.model_settings['vector_size']
