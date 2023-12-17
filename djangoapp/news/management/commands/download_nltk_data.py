a2589cd091d8f0611ab0396a60103ad939ee69e0
Adam Majczyk <adam.majczyk2001@gmail.com>
2023-12-17 15:58:18 +0100

    fixing nltk download in tests

--- djangoapp/news/management/commands/download_nltk_data.py
+++ djangoapp/news/management/commands/download_nltk_data.py
@@ -6,9 +6,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Downloading NLTK data...'))
-        nltk.download('punkt')
-        nltk.download('wordnet')
-        nltk.download('averaged_perceptron_tagger')
-        nltk.download('words')
-        nltk.download('stopwords')
+        try:
+            nltk.download('punkt')
+            nltk.download('wordnet')
+            nltk.download('averaged_perceptron_tagger')
+            nltk.download('words')
+            nltk.download('stopwords')
+        except:
+            self.stdout.write(self.style.ERROR('NLTK data download failed.'))
         self.stdout.write(self.style.SUCCESS('NLTK data downloaded successfully.'))
