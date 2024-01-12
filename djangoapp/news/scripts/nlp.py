381fe7ad60a57ffaf4f2d2a7f66e9b0a3244c99d
amajczyk <adam.majczyk2001@gmail.com>
2024-01-13 00:29:44 +0100

    clean code

--- djangoapp/news/scripts/nlp.py
+++ djangoapp/news/scripts/nlp.py
@@ -13,13 +13,11 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.tag import pos_tag
 import inflect
-import re 
+import re
 import copy as cp
 
 
-
-class NLP():
-
+class NLP:
     def __init__(self) -> None:
         w2v = self.Word2VecModel()
         predictive_model = self.PredictiveModel().model
@@ -35,42 +33,69 @@ class NLP():
         self.lemmatizer = WordNetLemmatizer()
 
         self.punct = string.punctuation
-        self.all_quoatation = ['“', '”', '‘', '’', '’', '‘', '’', '‘', u'\u2018', u'\u2019', u'\u201c', u'\u201d', u'\u2032', u'\u2033', '“',  '”',]
+        self.all_quoatation = [
+            "“",
+            "”",
+            "‘",
+            "’",
+            "’",
+            "‘",
+            "’",
+            "‘",
+            "\u2018",
+            "\u2019",
+            "\u201c",
+            "\u201d",
+            "\u2032",
+            "\u2033",
+            "“",
+            "”",
+        ]
 
-
-    class Word2VecModel():
+    class Word2VecModel:
         def __init__(self) -> None:
-            model_settings_path = os.path.join(settings.BASE_DIR, 'news', 'config', 'model_settings.json')
+            model_settings_path = os.path.join(
+                settings.BASE_DIR, "news", "config", "model_settings.json"
+            )
             self.model_settings = self.return_best_model(path=model_settings_path)
-            
-            w2v_path = os.path.join(settings.BASE_DIR, 'news', 'word2vec_models', self.model_settings['model_path'])
-        
+
+            w2v_path = os.path.join(
+                settings.BASE_DIR,
+                "news",
+                "word2vec_models",
+                self.model_settings["model_path"],
+            )
+
             self.model = Word2Vec.load(w2v_path)
 
-            self.is_skipgram = self.model_settings['is_skipgram']
-            self.window_size = self.model_settings['window_size']
-            self.vector_size = self.model_settings['vector_size']
+            self.is_skipgram = self.model_settings["is_skipgram"]
+            self.window_size = self.model_settings["window_size"]
+            self.vector_size = self.model_settings["vector_size"]
 
         def return_best_model(self, path: str):
             # Load the best model from the model_settings.json file (best_word2vec property)
             with open(path) as json_file:
                 model_settings = json.load(json_file)
-            return model_settings['best_word2vec']
+            return model_settings["best_word2vec"]
 
-    class PredictiveModel():
+    class PredictiveModel:
         def __init__(self) -> None:
-            model_path = os.path.join(settings.BASE_DIR, 'news', 'predictive_models', 'catboost.pkl')
-            self.model = pickle.load(open(model_path, 'rb'))
-        
-    class Scaler():
-        def __init__(self) -> None:
-            scaler_path = os.path.join(settings.BASE_DIR, 'news', 'predictive_models', 'scaler.pkl')
-            self.scaler = pickle.load(open(scaler_path, 'rb'))
+            model_path = os.path.join(
+                settings.BASE_DIR, "news", "predictive_models", "catboost.pkl"
+            )
+            self.model = pickle.load(open(model_path, "rb"))
 
-    def predict_on_text(self, title:str):
+    class Scaler:
+        def __init__(self) -> None:
+            scaler_path = os.path.join(
+                settings.BASE_DIR, "news", "predictive_models", "scaler.pkl"
+            )
+            self.scaler = pickle.load(open(scaler_path, "rb"))
+
+    def predict_on_text(self, title: str) -> np.ndarray:
         # print(text)
-        text = self.preprocess_title(pd.DataFrame({'title': [title]}))
-        text = self.get_word_vectors(self.w2v, text['title'][0], aggregation='mean')
+        text = self.preprocess_title(pd.DataFrame({"title": [title]}))
+        text = self.get_word_vectors(self.w2v, text["title"][0], aggregation="mean")
 
         # drop dimensions
         text = np.delete(text, self.dropped_dims)
@@ -78,79 +103,81 @@ class NLP():
         text = self.scaler.transform([text])
         # print(len(text))
         return self.predictive_model.predict_proba(text.reshape(1, -1))
-            
-    def get_dimensions_to_drop(self):
-        var_path = os.path.join(settings.BASE_DIR, 'news', 'predictive_models', 'worst_performing_dimensions_intersection.pkl')
+
+    def get_dimensions_to_drop(self) -> list:
+        var_path = os.path.join(
+            settings.BASE_DIR,
+            "news",
+            "predictive_models",
+            "worst_performing_dimensions_intersection.pkl",
+        )
         # read variables to be dropped from pickle file
-        with open(var_path, 'rb') as f:
+        with open(var_path, "rb") as f:
             variables_to_drop = pickle.load(f)
 
-        variables_to_drop = [x.replace('dim_', '') for x in variables_to_drop]
+        variables_to_drop = [x.replace("dim_", "") for x in variables_to_drop]
         variables_to_drop = [int(x) for x in variables_to_drop]
         return variables_to_drop
 
-    
-
-    def get_word_vectors(self, w2v_model:Word2VecModel, title:str, aggregation=None):
+    def get_word_vectors(
+        self, w2v_model: Word2VecModel, title: str, aggregation=None
+    ) -> np.ndarray:
         model = w2v_model.model
         word_vectors = [model.wv[word] for word in title if word in model.wv]
         # print(len(word_vectors))
         # print(word_vectors)
         if len(word_vectors) == 0:
             return np.zeros(model.vector_size)
-        elif aggregation == 'mean':
+        elif aggregation == "mean":
             return np.mean(word_vectors, axis=0)
         elif aggregation is None:
             return word_vectors
-        
-    def load_predictive_model(self, path:str):
-        classifier = pickle.load(open(path, 'rb'))
-        return classifier
-        
 
-    def get_stem_of_word(self, text:str):
+    def load_predictive_model(self, path: str) -> catboost.CatBoostClassifier:
+        classifier = pickle.load(open(path, "rb"))
+        return classifier
+
+    def get_stem_of_word(self, text: str) -> str:
         return self.stemmer.stem(text)
 
-    def lemmalize_word(self, text:str):
+    def lemmalize_word(self, text: str) -> str:
         return self.lemmatizer.lemmatize(text)
 
-    def remove_punct(self, text:str):
+    def remove_punct(self, text: str) -> str:
         for p in self.punct:
-            text = text.replace(p, '')
-        text = text.replace('...', ' ')
-        text = text.replace('…', ' ')
-        text = text.replace('—', ' ')
-        text = text.replace('–', ' ')
-        text = text.replace('–', ' ')
-        text = text.replace( '—', ' ')
+            text = text.replace(p, "")
+        text = text.replace("...", " ")
+        text = text.replace("…", " ")
+        text = text.replace("—", " ")
+        text = text.replace("–", " ")
+        text = text.replace("–", " ")
+        text = text.replace("—", " ")
 
         translator = str.maketrans("", "", string.punctuation)
         text = text.translate(translator)
 
         return text
 
-    def remove_possessive(self, text:str):
+    def remove_possessive(self, text: str) -> str:
         for p in self.all_quoatation:
-            text = text.replace(p+'s', '')
-            text = text.replace('s'+ p, '')
+            text = text.replace(p + "s", "")
+            text = text.replace("s" + p, "")
         return text
 
-    def remove_possessive_nltk(self, text:str):
+    def remove_possessive_nltk(self, text: str) -> str:
         words_ = word_tokenize(text)
         tagged_words = pos_tag(words_)
 
         updated_words = []
         for i, (word, pos) in enumerate(tagged_words):
-            if i > 0 and pos == 'POS' and tagged_words[i - 1][1] == 'NN':
+            if i > 0 and pos == "POS" and tagged_words[i - 1][1] == "NN":
                 continue
             updated_words.append(word)
 
-        result = ' '.join(updated_words)
+        result = " ".join(updated_words)
         return result
 
-
-
-    def expand_contractions_nltk(self, text:str):
+    def expand_contractions_nltk(self, text: str) -> str:
         words = word_tokenize(text)
         lemmatizer = WordNetLemmatizer()
 
@@ -160,40 +187,32 @@ class NLP():
             expanded_word = lemmatizer.lemmatize(word)
             expanded_words.append(expanded_word)
 
-        result = ' '.join(expanded_words)
+        result = " ".join(expanded_words)
         return result
 
-
-
-    import re
-
-    def replace_short_version(self, text:string):
+    def replace_short_version(self, text: string) -> string:
         for p in self.all_quoatation:
-            text = re.sub(r'\b' + p + 're\b', ' are', text)
-            text = re.sub(r'\b' + p + 've\b', ' have', text)
-            text = re.sub(r'\b' + p + 'll\b', ' will', text)
-            text = re.sub(r'\b' + p + 'm\b', ' am', text)
-            text = re.sub(r'\b' + p + 'd\b', ' would', text)
-            text = re.sub(r'\b' + 'n' + p + 't\b', ' not', text)
-        return text 
+            text = re.sub(r"\b" + p + "re\b", " are", text)
+            text = re.sub(r"\b" + p + "ve\b", " have", text)
+            text = re.sub(r"\b" + p + "ll\b", " will", text)
+            text = re.sub(r"\b" + p + "m\b", " am", text)
+            text = re.sub(r"\b" + p + "d\b", " would", text)
+            text = re.sub(r"\b" + "n" + p + "t\b", " not", text)
+        return text
 
-    def remove_words_from_file(self, text_list:list, file_name:str):
+    def remove_words_from_file(self, text_list: list, file_name: str):
         # print(text)
         with open(file_name) as f:
             words = f.readlines()
         words = [word.strip() for word in words]
         # print(words)
-    
+
         text = [word for word in text_list if word not in words]
 
-        return text   
+        return text
 
-
-
-
-
-    def replace_numbers_with_words(self, text:list):
-        regex = r'\b\d+\b'
+    def replace_numbers_with_words(self, text: list) -> str:
+        regex = r"\b\d+\b"
 
         matched = re.finditer(regex, text)
 
@@ -203,73 +222,64 @@ class NLP():
             text = text.replace(number, p.number_to_words(number))
         return text
 
-
-
-    def tokenize(self, text:list):
-        stop_words = set(stopwords.words('english'))
+    def tokenize(self, text: list) -> list:
+        stop_words = set(stopwords.words("english"))
         return [word for word in word_tokenize(text.lower()) if word not in stop_words]
 
+    def remove_non_ascii_characters_from_text(self, text: list) -> str:
+        return "".join([i if ord(i) < 128 else " " for i in text])
 
-
-
-    def remove_non_ascii_characters_from_text(self, text:list):
-        return ''.join([i if ord(i) < 128 else ' ' for i in text])
-
-    def remove_non_ascii_characters_from_list(self, text_list:list):
+    def remove_non_ascii_characters_from_list(self, text_list: list) -> list:
         return [self.remove_non_ascii_characters_from_text(text) for text in text_list]
 
-
-
-    def remove_non_eng_words(self, text_list:list, words:list = nltk_corpus_words):
+    def remove_non_eng_words(
+        self, text_list: list, words: list = nltk_corpus_words
+    ) -> list:
         return [word for word in text_list if word.lower() in words]
-        
 
-
-    def preprocess_title(self, df:pd.DataFrame, verbose = False):
-
+    def preprocess_title(self, df: pd.DataFrame, verbose=False) -> pd.DataFrame:
         # df = cp.deepcopy(df)
-        
+
         english_words = set(w.lower() for w in nltk_corpus_words.words())
 
         # remove punctuation and other stuff
         if verbose:
-            print(df['title'])
-            print("Removing numbers and replacing with words...")   
-        df['title'] = df['title'].apply(self.replace_numbers_with_words)
-        
+            print(df["title"])
+            print("Removing numbers and replacing with words...")
+        df["title"] = df["title"].apply(self.replace_numbers_with_words)
+
         # remove possesive s
         if verbose:
-            print(df['title'])
+            print(df["title"])
             print("Removing possesive s...")
-        df['title'] = df['title'].apply(self.remove_possessive)
+        df["title"] = df["title"].apply(self.remove_possessive)
 
         # replace short versions
         if verbose:
-            print(df['title'])
+            print(df["title"])
             print("Expanding short versions...")
-        df['title'] = df['title'].apply(self.replace_short_version)
+        df["title"] = df["title"].apply(self.replace_short_version)
 
         # remove punctuation
         if verbose:
-            print(df['title'])
-            print("Removing punctuation...")   
-        df['title'] = df['title'].apply(self.remove_punct)
-
+            print(df["title"])
+            print("Removing punctuation...")
+        df["title"] = df["title"].apply(self.remove_punct)
 
         # replace US with USA
         if verbose:
-            print(df['title'])
+            print(df["title"])
             print("Replacing US with USA...")
-        df['title'] = df['title'].apply(lambda x: x.replace('US', 'USA'))
+        df["title"] = df["title"].apply(lambda x: x.replace("US", "USA"))
 
         # tokenize
         if verbose:
-            print(df['title'])
+            print(df["title"])
             print("Tokenizing...")
-            
-        df['title'] = df['title'].apply(self.tokenize)
+
+        df["title"] = df["title"].apply(self.tokenize)
         # print(df['title'])
-        
+
         # remove words in words_to_remove.txt
         # if verbose:
         #     print(df['title'])
@@ -281,19 +291,20 @@ class NLP():
         # print("Stemming words...")
         # df['title'] = df['title'].apply(lambda x: [get_stem_of_word(word) for word in x])
 
-        
         # lemmalize words
         if verbose:
-            print(df['title'])
+            print(df["title"])
             print("Lemmalizing words...")
-        df['title'] = df['title'].apply(lambda x: [self.lemmalize_word(word) for word in x])
+        df["title"] = df["title"].apply(
+            lambda x: [self.lemmalize_word(word) for word in x]
+        )
 
         # remove non ascii characters
         if verbose:
-            print(df['title'])
+            print(df["title"])
             print("Removing non ascii characters...")
-            
-        df['title'] = df['title'].apply(self.remove_non_ascii_characters_from_list)
+
+        df["title"] = df["title"].apply(self.remove_non_ascii_characters_from_list)
 
         # remove non english words
         # if verbose:
@@ -303,24 +314,21 @@ class NLP():
 
         # remove rows with empty titles
         if verbose:
-            print(df['title'])
+            print(df["title"])
             print("Removing empty titles...")
-            
-        df = df[df['title'].apply(len) > 0]
+
+        df = df[df["title"].apply(len) > 0]
 
         # remove stopwords one more time
         if verbose:
-            print(df['title'])
+            print(df["title"])
             print("Removing stopwords one more time...")
-        stop_words = set(stopwords.words('english'))
-        df['title'] = df['title'].apply(lambda x: [word for word in x if word not in stop_words])
-
+        stop_words = set(stopwords.words("english"))
+        df["title"] = df["title"].apply(
+            lambda x: [word for word in x if word not in stop_words]
+        )
 
         # remove spaces
-        df['title'] = df['title'].apply(lambda x: [word for word in x if word != ' '])
-
-
-
+        df["title"] = df["title"].apply(lambda x: [word for word in x if word != " "])
 
         return df
-
