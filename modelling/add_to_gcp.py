b004c7425652da517ff5e281c62e73470ad48aa5
Adam <adam.majczyk2001@gmail.com>
2024-01-16 18:21:07 +0100

    cd

--- modelling/add_to_gcp.py
+++ modelling/add_to_gcp.py
@@ -27,10 +27,41 @@ def upload_folder_to_gcs(bucket_name, folder_path):
             print(f"Uploaded {local_file_path} to modelling/{local_file_path}")
 
 
+
+def upload_file_to_gcs(bucket_name, local_file_path):
+    # Create a client
+    client = storage.Client()
+
+    # Get the bucket
+    bucket = client.get_bucket(bucket_name)
+
+    # replace \ with / (windows -> unix)
+    local_file_path = local_file_path.replace("\\", "/")
+
+    # Create a blob object
+    blob = bucket.blob("modelling/" + local_file_path)
+
+    # Upload the file to GCS
+    blob.upload_from_filename(local_file_path)
+
+    print(f"Uploaded {local_file_path} to modelling/{local_file_path}")
+
+
 # Usage example
 bucket_name = "clickbait-detector-bucket"
-folder_paths = ["clickbait_dataset", "clickbait_dataset_more_variables", "fake_news"]
+folder_paths = [
+    "clickbait_dataset", 
+    "clickbait_dataset_more_variables", 
+    "fake_news", 
+    ]
+
+file_paths = [
+    "model_testing\\vertex_results.csv"
+]
 
 
 for folder_path in folder_paths:
     upload_folder_to_gcs(bucket_name, folder_path)
+
+for file_path in file_paths:
+    upload_file_to_gcs(bucket_name, file_path)
