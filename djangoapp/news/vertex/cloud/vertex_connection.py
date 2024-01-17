ac34e1af7a59e0625e83139f352b4fc7b9c16de8
alexmal01 <almal@AlexLenovo>
2024-01-17 22:46:07 +0100

    fix vertex tests v3

--- djangoapp/news/vertex/cloud/vertex_connection.py
+++ djangoapp/news/vertex/cloud/vertex_connection.py
@@ -1,23 +1,15 @@
-"""Vertex AI connection module."""
 import threading
 from typing import Optional
 from enum import Enum
-from dataclasses import asdict
-
-
+from google.auth import default, load_credentials_from_dict
 from news.vertex.configs.config import load_config_from_file
 from vertexai.language_models import TextGenerationModel
 from vertexai.preview import generative_models as gen
-from google.auth import default, load_credentials_from_dict
+from dataclasses import asdict
 from google.cloud import aiplatform
 
 
 class ModelName(Enum):
-    """Vertex AI model garden model names.
-
-    See https://cloud.google.com/vertex-ai/docs/start/explore-models
-    for more information.
-    """
     BISON_001 = "text-bison@001"
     UNICORN_001 = "text-unicorn@001"
     BISON = "text-bison"
@@ -29,8 +21,7 @@ TITLE_PLACEHOLDER = "PLACE_FOR_TITLE"
 SUMMARY_PLACEHOLDER = "PLACE_FOR_SUMMARY"
 
 
-class VertexAI:  # pylint: disable=too-many-instance-attributes
-    """Vertex AI class."""
+class VertexAI:
     __slots__ = [
         "project_id",
         "location",
@@ -46,7 +37,7 @@ class VertexAI:  # pylint: disable=too-many-instance-attributes
         "safety",
     ]
 
-    def __init__(  # pylint: disable=too-many-arguments
+    def __init__(
         self,
         project_id: Optional[str] = None,
         location: Optional[str] = None,
@@ -57,8 +48,8 @@ class VertexAI:  # pylint: disable=too-many-instance-attributes
         service_account: Optional[str] = None,
         model_name: ModelName = ModelName.GEMINI,
         title: str = "This is the Most Clickbait Title Ever!",
-        prompt: str = "Is this title a clickbait: 'PLACE_FOR_TITLE'? Summary of the article: "
-        "'PLACE_FOR_SUMMARY'. Return 1 if yes, 0 if no.",
+        prompt: str = f"Is this title a clickbait: 'PLACE_FOR_TITLE'? Summary of the article: "
+        f"'PLACE_FOR_SUMMARY'. Return 1 if yes, 0 if no.",
         safety: bool = False,
     ):
         self.my_chat_model = None
@@ -81,15 +72,14 @@ class VertexAI:  # pylint: disable=too-many-instance-attributes
             }
         else:
             self.safety = {
-                gen.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: gen.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,  # pylint: disable=line-too-long
-                gen.HarmCategory.HARM_CATEGORY_HARASSMENT: gen.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,  # pylint: disable=line-too-long
-                gen.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: gen.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,  # pylint: disable=line-too-long
-                gen.HarmCategory.HARM_CATEGORY_HATE_SPEECH: gen.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,  # pylint: disable=line-too-long
+                gen.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: gen.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+                gen.HarmCategory.HARM_CATEGORY_HARASSMENT: gen.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+                gen.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: gen.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+                gen.HarmCategory.HARM_CATEGORY_HATE_SPEECH: gen.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
             }
         self.init_connection()
 
     def init_connection(self):
-        """Initialize Vertex AI connection."""
         aiplatform.init(
             project=self.project_id,
             location=self.location,
@@ -101,29 +91,25 @@ class VertexAI:  # pylint: disable=too-many-instance-attributes
         )
 
     def load_config(self):
-        """Load Vertex AI configuration."""
         try:
             self.credentials, self.project_id = load_credentials_from_dict(
                 asdict(load_config_from_file())
             )
-        except (FileNotFoundError, KeyError):
+        except FileNotFoundError or KeyError:
             self.credentials, self.project_id = default()
 
     def load_model(self):
-        """Load Vertex AI model."""
         if self.model_name == ModelName.GEMINI:
             self.my_chat_model = gen.GenerativeModel(self.model_name.value)
             return
         self.my_chat_model = TextGenerationModel.from_pretrained(self.model_name.value)
 
     def predict(self):
-        """Predict the clickbait score."""
         if self.model_name == ModelName.GEMINI:
             return self.predict_gemini()
         return self.my_chat_model.predict(self.prompt).text
 
     def predict_gemini(self):
-        """Predict the clickbait score using Gemini."""
         return self.my_chat_model.generate_content(
             self.prompt,
             generation_config={"temperature": 0.3},
@@ -131,11 +117,9 @@ class VertexAI:  # pylint: disable=too-many-instance-attributes
         ).text
 
     def run(self, title, summary=None):
-        """Run the model."""
         if summary:
             self.title = title
-            self.prompt = (f"Is this title a clickbait: '{title}'?"
-                           f"Summary of the article: '{summary}'. Return 1 if yes, 0 if no.")
+            self.prompt = f"Is this title a clickbait: '{title}'? Summary of the article: '{summary}'. Return 1 if yes, 0 if no."
         else:
             self.title = title
             self.prompt = (
@@ -144,17 +128,15 @@ class VertexAI:  # pylint: disable=too-many-instance-attributes
         self.load_config()
         self.load_model()
         prediction = self.predict()
-        return_value = prediction.strip() != "0"
+        return_value = False if prediction.strip() == "0" else True
         return return_value
 
 
 def runner(vertex_ai: VertexAI, title: str):
-    """Run the model in a thread."""
     vertex_ai.run(title=title)
 
 
 def main():
-    """Main function."""
     titles = [
         "You have to see this!",
         "Presidential election results",
