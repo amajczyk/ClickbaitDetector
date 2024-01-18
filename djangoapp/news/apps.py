d01d48da58e1a8b79ae877a8b4ed182174012849
alexmal01 <almal@AlexLenovo>
2024-01-18 17:41:56 +0100

    revert apps.py

--- djangoapp/news/apps.py
+++ djangoapp/news/apps.py
@@ -1,29 +1,32 @@
-"""App configuration for the news app."""
+import os
 
 from django.apps import AppConfig
-from django.core import management
 from news.scripts.model_loader import ModelLoader
 
 
 class NewsConfig(AppConfig):
-    """App configuration for the news app."""
     name = "news"
+
+    def __init__(self, app_name, app_module):
+        super().__init__(app_name, app_module)
 
     def ready(self):
         # Run the download_nltk_data management command when the app is ready
+        from django.core import management
+
         management.call_command("download_nltk_data")
 
         print("Initial model loading....")
         model_loader = ModelLoader()  # wake up modules
         # this hard codeed url may cause problems in the future, but it's enough for now
-        url = "https://www.thesun.co.uk/news/19747379/queen-elizabeth-dead-news/"
+        URL = "https://www.thesun.co.uk/news/19747379/queen-elizabeth-dead-news/"
 
         print("Initial model loading finished.")
         print("Performing mock prediction...")
 
         try:
             print("Scraping...")
-            scraped_data = model_loader.scraper.scrape(url)
+            scraped_data = model_loader.scraper.scrape(URL)
             title = scraped_data["title"]
             print("Using NLP model...")
             model_loader.nlp.predict_on_text(title)
@@ -35,7 +38,7 @@ class NewsConfig(AppConfig):
             )
             print("Using VertexAI model...")
             model_loader.vertex.run(scraped_data["title"], summary)
-        except (KeyError, TypeError, AttributeError) as e:
-            print("Error in initialisation: ", e)
+        except Exception as e:
+            print("Erorr in initialisation: ", e)
             print("The app starts normally, but there may be some problems.")
-        print("Initialisation finished.")
+        print("Initilisation finished.")
